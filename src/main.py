a97f37bf238698b9e12201aa702d2d0d9b704a31
Alexey Fedechkin <alex@centralhardware.me>
2024-01-12 20:37:45 +0100

    fix inserting

--- src/main.py
+++ src/main.py
@@ -94,7 +94,7 @@ async def handler(event):
     t = await get_admins(event.chat)
     if event.raw_text != '':
         logging.info("%s: %s %s (%s)", chat_title, event.raw_text, t[1], t[0])
-        data = [[datetime.now(), event.raw_text, chat_title, chat_id, event.chat_id, t[1]], t[0]]
+        data = [[datetime.now(), event.raw_text, chat_title, chat_id, event.chat_id, t[1], t[0]]]
         clickhouse.insert('telegram_messages_new', data,
                           ['date_time', 'message', 'title', 'username', 'id', 'members_count', 'admins2'])
     else:
