8ea7e24c6cd2bf120eb8ee311a91872478407566
alex <alex@centralhardware.me>
2024-03-29 08:18:41 +0100

    log message

--- src/main.py
+++ src/main.py
@@ -113,7 +113,7 @@ async def admin2(event):
 
 @client.on(events.NewMessage(incoming=True))
 async def handler(event):
-    logging.info(f"log message in chat {event.chat.title} {event.message.id}")
+    logging.info(f"log message in chat {event.chat.title} {event.message.id} {event.raw_text}")
 
     if event.chat_id >= 0 or event.is_private is True or event.raw_text == '' or event.message.sender is None: return
 
