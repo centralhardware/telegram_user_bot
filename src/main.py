1364ed3769702d39675d88eadae5573fbf5daee6
alex <alex@centralhardware.me>
2024-03-30 09:14:56 +0100

    pretty print chat title

--- src/main.py
+++ src/main.py
@@ -113,7 +113,7 @@ async def admin2(event):
 
 @client.on(events.NewMessage(incoming=True))
 async def handler(event):
-    logging.info(f"{event.message.id:,<13} {event.chat.title:<20s} {event.raw_text}")
+    logging.info(f"{event.message.id:13,} {event.chat.title:<20s} {event.raw_text}")
 
     if event.chat_id >= 0 or event.is_private is True or event.raw_text == '' or event.message.sender is None: return
 
