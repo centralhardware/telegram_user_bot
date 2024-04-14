699ac3c7c96a44f827758120764b634f12d0cf1c
alex <alex@centralhardware.me>
2024-04-14 19:37:10 +0200

    fix attribute error

--- src/admin_utils.py
+++ src/admin_utils.py
@@ -1,10 +1,12 @@
 from telethon.tl.types import ChatParticipantCreator
 
+
 def build_username(user):
     if user.username is not None:
         return "@" + user.username
     else:
         return "@" + user.usernames[0].username
+
 
 # The function retrieves administrators from a chat
 async def get_admins(chat, client):
@@ -15,11 +17,8 @@ async def get_admins(chat, client):
             count += 1
             if user.bot:
                 continue
-            if (isinstance(user.participant, ChatParticipantCreator)
-                or (hasattr(user.participant, 'admin_rights') and user.participant.admin_rights.delete_messages)):
+            if isinstance(user.participant, ChatParticipantCreator) or user.participant.admin_rights.delete_messages:
                 admins.append(build_username(user))
-        except AttributeError as e:
-            print(f'AttributeError occurred: {str(e)}')
-        except TypeError as e:
-            print(f'TypeError occurred: {str(e)}')
+        except Exception:
+            pass
     return [admins, count]
