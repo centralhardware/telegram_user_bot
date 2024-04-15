5b7a968393630b91a5e49004c2dee23c393b1e2e
alex <alex@centralhardware.me>
2024-04-16 00:52:36 +0200

    flsuh every 10 seconds

--- src/Accumulator.py
+++ src/Accumulator.py
@@ -1,6 +1,5 @@
 import logging
 import time
-from collections import deque
 
 
 class Accumulator:
@@ -10,34 +9,24 @@ class Accumulator:
         self.total_items_added = 0
         self.external_function = external_function
         self.start_time = time.time()
-        self.last_flush_time = 0
-        self.flush_times = deque(maxlen=5)
+        self.next_flush_time = self.start_time + 10
 
     def add(self, item):
-        if len(self.data) < self.item_limit:
+        current_time = time.time()
+        if current_time >= self.next_flush_time or len(self.data) >= self.item_limit:
+            self.flush()
+            self.start_time = current_time
+            self.next_flush_time = self.start_time + 10  # Update the next flush time
+        else:
             self.data.append(item)
             self.total_items_added += 1
-        else:
-            self.flush()
-            self.start_time = time.time()
 
     def flush(self):
         self.external_function(self.data)
-        flush_time = time.time() - self.start_time
-        self.flush_times.append(flush_time)
-        adjustment = int(self.item_limit * 0.1)
-        average_flush_time = sum(self.flush_times) / len(self.flush_times)
-        if self.last_flush_time:
-            if average_flush_time < self.last_flush_time:
-                self.item_limit += adjustment
-                logging.info(f'Resizing the item limit. New limit: {self.item_limit}')
-            elif average_flush_time > self.last_flush_time:
-                self.item_limit = max(50, self.item_limit - adjustment)
-                logging.info(f'Resizing the item limit. New limit: {self.item_limit}')
-        self.last_flush_time = flush_time
         logging.info(
-            f'Data flushed. Time taken: {round(flush_time, 2)}s. New item limit: {self.item_limit}. Avg speed: {flush_time / (self.total_items_added if self.total_items_added > 0 else 1)} items/s')
+            f'Data flushed. Avg speed: {10 / (self.total_items_added if self.total_items_added > 0 else 1)} items/s')
         self.data.clear()
 
     def get_len(self):
         return len(self.data)
+
