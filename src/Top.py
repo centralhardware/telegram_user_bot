8aeacafb6943434caaa0c4abffb026f66df9dccf
Fedechkin Aleksei <alex@centralhardware.me>
2024-05-28 16:41:37 +0200

    add top

--- src/Top.py
+++ src/Top.py
@@ -32,4 +32,4 @@ async def top(event):
 
         msg = msg + f"{res.result_rows.index(row) + 1}: {username} - {row[1]}\n"
 
-    await event.client.send_message(event.chat, msg, reply_to=event.message.reply_to_msg_id)
+    await event.client.send_message(event.chat, msg, reply_to=event.message.id)
