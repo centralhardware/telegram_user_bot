9abd4ea36b712fece84c0d8fd55764fcc0d9c37f
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-04 23:29:38 +0200

    Updated log message color in scrapper.py

--- src/scrapper.py
+++ src/scrapper.py
@@ -119,8 +119,6 @@ async def save_deleted(event):
             FROM chats_log
             WHERE chat_id = {chat_id:Int64} and message_id = {message_id:Int64}
             """, {'chat_id': event.chat_id, 'message_id': msg_id})
+            logging.info(colored(f" Deleted {res.result_rows[0][0]} {msg_id} {res.result_rows[0][1]}", 'yellow'))
         except Exception:
             logging.info(colored(f" Deleted {event.chat_id} {msg_id}", 'yellow'))
-            return
-
-        logging.info(colored(f" Deleted {res.result_rows[0][0]} {msg_id} {res.result_rows[0][1]}", 'yellow'))
