1cb619f5f5f32690633d3e7e57e0a4bf7e06ea17
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-05 22:47:24 +0200

    Simplify AI response and remove message deletion

--- src/Ai.py
+++ src/Ai.py
@@ -10,10 +10,9 @@ model = genai.GenerativeModel(model_name='gemini-1.5-flash')
 
 async def answer(event):
     query = event.raw_text.replace('!ai', '')
-    response = model.generate_content(f"ответь на языке вопроса коротко максимально аргументировано на следующее сообщение, добавиь в конце ремарку какой версией AI это сообщение сгенерировано: {query}")
+    response = model.generate_content(f"ответь на языке вопроса коротко максимально аргументировано на следующее сообщение: {query}")
     logging.info(f"ask ai {query} answer {response.text}")
-    await event.client.delete_messages(event.chat, message_ids=[event.message.id])
     if event.message.reply_to_msg_id is not None:
-        await event.client.send_message(event.chat, response.text, reply_to=event.message.reply_to_msg_id)
+        await event.client.send_message(event.chat, response.text + '\n\n gemini AI', reply_to=event.message.reply_to_msg_id)
     else:
-        await event.client.send_message(event.chat, response.text, reply_to=event.message.id)
+        await event.client.send_message(event.chat, response.text + '\n\n gemini AI', reply_to=event.message.id)
