2c9219ba61135daf4b55e6d56d1cba5b08ce8af2
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-05 23:20:20 +0200

    Update AI model version

--- src/Ai.py
+++ src/Ai.py
@@ -10,7 +10,7 @@ model = genai.GenerativeModel(model_name='gemini-1.5-pro-latest')
 
 async def answer(event):
     query = event.raw_text.replace('!ai', '')
-    response = model.generate_content(f"Представь что ты самый лучший в мире эксперт в ИТ и ответь на заданный тебе вопрос одним сообщением аргументировано на языке вопроса стараясь чтобы все было понятно после прочтения минимизируй размер ответа без потери смысла старайся использовать минимальной количество строчек: {query}")
+    response = model.generate_content(f"Представь что ты самый лучший в мире эксперт в ИТ и ответь на заданный тебе вопрос одним сообщением аргументировано на языке вопроса стараясь чтобы все было понятно после прочтения минимизируй размер ответа без потери смысла старайся использовать минимальной количество строчек но сохраняя принятое форматирование кода: {query}")
     logging.info(f"ask ai {query} answer {response.text}")
     if event.message.reply_to_msg_id is not None:
         await event.client.send_message(event.chat, response.text + '\n\n gemini AI', reply_to=event.message.reply_to_msg_id)
