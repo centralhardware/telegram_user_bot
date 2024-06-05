6ab99f5f0a770705cf6fbb1c3fe501ac02db3ab5
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-05 17:29:11 +0200

    add ai

--- src/Ai.py
+++ src/Ai.py
@@ -12,6 +12,7 @@ async def answer(event):
     query = event.raw_text.replace('!ai', '')
     response = model.generate_content(f"ответь на языке вопроса коротко максимально аргументировано на следующее сообщение, добавиь в конце ремарку какой версией AI это сообщение сгенерировано: {query}")
     logging.info(f"ask ai {query} answer {response.text}")
+    await event.client.delete_messages(event.chat, message_ids=[event.message.id])
     if event.message.reply_to_msg_id is not None:
         await event.client.send_message(event.chat, response.text, reply_to=event.message.reply_to_msg_id)
     else:
