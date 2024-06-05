c03a8be547143645d4b8b651f92bedc3d8c0e4cd
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-06 01:01:57 +0200

    Update AI model version

--- src/TelegramUtils.py
+++ src/TelegramUtils.py
@@ -1,5 +1,7 @@
 from telethon import TelegramClient
 
+from config import config
+
 
 def create_telegram_client(session_name, phone):
     c = TelegramClient(session_name, config.api_id, config.api_hash)
