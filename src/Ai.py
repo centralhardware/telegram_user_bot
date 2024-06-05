186175ecfe05a939c9eeaff0ffb894f56697ab52
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-05 17:15:59 +0200

    add ai

--- src/Ai.py
+++ src/Ai.py
@@ -7,9 +7,9 @@ from config import config
 genai.configure(api_key=config.gemini_api_key)
 model = genai.GenerativeModel(model_name='gemini-1.5-flash')
 
+
 async def answer(event):
-    reply_msg = await event.client.get_messages(event.chat_id, ids=event.message.reply_to_msg_id)
-    logging.info(f"ask ai {reply_msg.raw_text}")
-    response = model.generate_content(f"ответь на языке вопроса коротко максимально аргументировано на следующее сообщение, добавиь в конце ремарку какой версией AI это сообщение сгенерировано: {reply_msg.raw_text}")
-    logging.info(f"ask ai {reply_msg.raw_text} answer {response.text}")
-    await event.client.edit_message(event.message, response.text)
+    query = event.raw_text.split(':')[1]
+    response = model.generate_content(f"ответь на языке вопроса коротко максимально аргументировано на следующее сообщение, добавиь в конце ремарку какой версией AI это сообщение сгенерировано: {query}")
+    logging.info(f"ask ai {query} answer {response.text}")
+    await event.client.send_message(event.chat, response.text, reply_to=event.message.id)
