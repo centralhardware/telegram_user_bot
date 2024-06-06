ee450da28cbd38279d13610b66b04bcaf4eff769
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-06 07:56:46 +0200

    Update AI model version

--- src/main.py
+++ src/main.py
@@ -15,16 +15,16 @@ from web import MessageSender
 
 client = create_telegram_client('session/alex', config.telephone)
 
-client.add_event_handler(save_outgoing, events.NewMessage(outgoing=True))
-client.add_event_handler(save_deleted, events.MessageDeleted())
-client.add_event_handler(save_incoming, events.NewMessage(incoming=True))
-client.add_event_handler(notify_admins, events.NewMessage(outgoing=True, pattern='!n', forwards=False))
-client.add_event_handler(top, events.NewMessage(outgoing=True, pattern='!top', forwards=False, chats=[-1001633660171]))
-client.add_event_handler(top, events.NewMessage(incoming=True, pattern='!top', forwards=False, chats=[-1001633660171]))
-client.add_event_handler(deleted, events.NewMessage(outgoing=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
-client.add_event_handler(deleted, events.NewMessage(incoming=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
+# client.add_event_handler(save_outgoing, events.NewMessage(outgoing=True))
+# client.add_event_handler(save_deleted, events.MessageDeleted())
+# client.add_event_handler(save_incoming, events.NewMessage(incoming=True))
+# client.add_event_handler(notify_admins, events.NewMessage(outgoing=True, pattern='!n', forwards=False))
+# client.add_event_handler(top, events.NewMessage(outgoing=True, pattern='!top', forwards=False, chats=[-1001633660171]))
+# client.add_event_handler(top, events.NewMessage(incoming=True, pattern='!top', forwards=False, chats=[-1001633660171]))
+# client.add_event_handler(deleted, events.NewMessage(outgoing=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
+# client.add_event_handler(deleted, events.NewMessage(incoming=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
 client.add_event_handler(answer, events.NewMessage(outgoing=True, pattern='!ai', forwards=False))
-client.add_event_handler(answer, events.NewMessage(incoming=True, pattern='!ai', forwards=False, chats=[-1001633660171]))
+# client.add_event_handler(answer, events.NewMessage(incoming=True, pattern='!ai', forwards=False, chats=[-1001633660171]))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(message)s')
