de3594302d61a09ecb83f55ce8fcf9b90e2c8f1d
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-06 07:36:48 +0200

    Update AI model version

--- src/Ai.py
+++ src/Ai.py
@@ -7,15 +7,25 @@ from TelegramUtils import client2
 
 genai.configure(api_key=config.gemini_api_key)
 
+async def answerPublic(event):
+    answer(event, False)
 
-async def answer(event):
+
+async def anserPrivate(event):
+    answer(event, True)
+
+
+async def answer(event, is_private):
     model = genai.GenerativeModel(model_name='gemini-1.5-pro-latest')
     query = event.raw_text.replace('!ai', '')
     response = model.generate_content(
         f"Представь что ты самый лучший в мире эксперт в ИТ и ответь на заданный тебе вопрос одним сообщением аргументировано на языке вопроса стараясь чтобы все было понятно после прочтения минимизируй размер ответа без потери смысла старайся использовать минимальной количество строчек но сохраняя принятое форматирование кода: {query}")
     logging.info(f"ask ai {query} answer {response.text}")
-    if event.message.reply_to_msg_id is not None:
-        await client2.send_message(event.chat.id, response.text + '\n\n gemini AI',
-                                   reply_to=event.message.reply_to_msg_id)
+    if (is_private):
+        if event.message.reply_to_msg_id is not None:
+            await client2.send_message(event.chat.id, response.text + '\n\n gemini AI',
+                               reply_to=event.message.reply_to_msg_id)
+        else:
+            await client2.send_message(event.chat.id, response.text + '\n\n gemini AI', reply_to=event.message.id)
     else:
-        await client2.send_message(event.chat.id, response.text + '\n\n gemini AI', reply_to=event.message.id)
+        await event.client.edit_message(event.message, response.text + '\n\n gemini AI')
