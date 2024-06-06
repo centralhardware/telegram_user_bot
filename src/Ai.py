a4991fb6faa2aafb21b7f0c9887128c1151fc215
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-06 07:45:31 +0200

    Update AI model version

--- src/Ai.py
+++ src/Ai.py
@@ -7,21 +7,14 @@ from TelegramUtils import client2
 
 genai.configure(api_key=config.gemini_api_key)
 
-async def answerPublic(event):
-    await answer(event, False)
 
-
-async def anserPrivate(event):
-    await answer(event, True)
-
-
-async def answer(event, is_private):
+async def answer(event):
     model = genai.GenerativeModel(model_name='gemini-1.5-pro-latest')
     query = event.raw_text.replace('!ai', '')
     response = model.generate_content(
         f"Представь что ты самый лучший в мире эксперт в ИТ и ответь на заданный тебе вопрос одним сообщением аргументировано на языке вопроса стараясь чтобы все было понятно после прочтения минимизируй размер ответа без потери смысла старайся использовать минимальной количество строчек но сохраняя принятое форматирование кода: {query}")
     logging.info(f"ask ai {query} answer {response.text}")
-    if (is_private):
+    if event.chait.id == -1001633660171:
         if event.message.reply_to_msg_id is not None:
             await client2.send_message(event.chat.id, response.text + '\n\n gemini AI',
                                reply_to=event.message.reply_to_msg_id)
