35788ca287d513deee1431108ed16da4b7387ade
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-12 20:14:51 +0200

    Handle exceptions in AI response generation

--- src/Ai.py
+++ src/Ai.py
@@ -14,7 +14,12 @@ async def answer(event):
     query = event.raw_text.replace('!ai', '')
     response = model.generate_content(
         f"ты лаконичный ассистент, который отвечает точно: {query}")
-    res = textwrap.wrap(response.text, 4000, break_long_words=True, replace_whitespace=False)
+    try:
+        res = textwrap.wrap(response.text, 4000, break_long_words=True, replace_whitespace=False)
+    except Exception:
+        await client2.send_message(event.chat_id, response.prompt_feedback, reply_to=event.message.id)
+        await client2.send_message(event.chat_id, response.candidates[0].finish_reason, reply_to=event.message.id)
+        await client2.send_message(event.chat_id, response.candidates[0].safety_ratings, reply_to=event.message.id)
     logging.info(f"ask ai {query} answer {response.text}")
     if event.chat_id == -1001633660171:
         if event.message.reply_to_msg_id is not None:
