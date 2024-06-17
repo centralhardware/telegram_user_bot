255f38f5280548114d41af3d48dfab4fe058f5cd
Fedechkin Aleksei <alex@centralhardware.me>
2024-06-17 21:27:12 +0200

    Reduce message count limit in get_messages function

--- src/Ai.py
+++ src/Ai.py
@@ -22,7 +22,7 @@ file_cache = {}
 
 async def get_messages(message, client, res, count=0):
     reply = await client.get_messages(message.chat.id, ids=message.reply_to_msg_id)
-    if reply is None or isinstance(reply, TotalList) or count >= 1000:
+    if reply is None or isinstance(reply, TotalList) or count >= 120:
         return res
 
     user = await client.get_entity(reply.from_id)
