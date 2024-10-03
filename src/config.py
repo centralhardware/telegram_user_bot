ff00cd0d07376e664f758d87ee9a5eb384ed6ab3
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-03 21:32:31 +0200

    update enviroments veriables names

--- src/config.py
+++ src/config.py
@@ -7,9 +7,9 @@ class Config:
         self.api_hash = os.getenv('API_HASH')
         self.telephone = os.getenv('TELEPHONE')
         self.telephone2 = os.getenv('TELEPHONE2')
-        self.db_user = os.getenv("production.CLICKHOUSE_USER")
-        self.db_password = os.getenv("production.CLICKHOUSE_PASSWORD")
-        self.db_host = os.getenv("production.CLICKHOUSE_HOST")
+        self.db_user = os.getenv("CLICKHOUSE_USER")
+        self.db_password = os.getenv("CLICKHOUSE_PASSWORD")
+        self.db_host = os.getenv("CLICKHOUSE_HOST")
         self.db_database = os.getenv("CLICKHOUSE_DATABASE")
 
 
