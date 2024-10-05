ddd9606ea34bb6560fbb063bccee5dd246f0501c
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-05 19:00:10 +0200

    drop web

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import logging
 
-from aiohttp import web
 from telethon import events
 
 from config import config
@@ -9,8 +8,6 @@ from scrapper import save_outgoing, save_incoming, save_deleted
 from Top import top
 from Deleted import deleted
 from TelegramUtils import create_telegram_client
-from web import MessageSender
-
 
 client = create_telegram_client('session/alex', config.telephone)
 client2 = create_telegram_client('session/alex2', config.telephone2)
@@ -21,16 +18,11 @@ client.add_event_handler(save_incoming, events.NewMessage(incoming=True))
 client.add_event_handler(notify_admins, events.NewMessage(outgoing=True, pattern='!n', forwards=False))
 client2.add_event_handler(top, events.NewMessage(outgoing=True, pattern='!top', forwards=False, chats=[-1001633660171]))
 client2.add_event_handler(top, events.NewMessage(incoming=True, pattern='!top', forwards=False, chats=[-1001633660171]))
-client2.add_event_handler(deleted, events.NewMessage(outgoing=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
-client2.add_event_handler(deleted, events.NewMessage(incoming=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
+client2.add_event_handler(deleted,
+                          events.NewMessage(outgoing=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
+client2.add_event_handler(deleted,
+                          events.NewMessage(incoming=True, pattern='!deleted', forwards=False, chats=[-1001633660171]))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(message)s')
     logging.info('start application')
-
-    sender = MessageSender(client)
-    app = web.Application()
-    app.add_routes([web.post('/', sender.handle_post)])
-    web.run_app(app, port=8080, loop=client.loop)
-
-    client.disconnect()
