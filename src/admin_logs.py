e71475165757d0c1fe9317e99a8876a825988e7e
Fedechkin Aleksei <alex@centralhardware.me>
2025-04-16 07:31:51 +0200

    add support for recent actions savings

---
+++ src/admin_logs.py
@@ -0,0 +1,77 @@
+import json
+import logging
+from datetime import datetime
+
+import clickhouse_connect
+from telethon.tl.functions.channels import GetAdminLogRequest
+
+from config import config
+
+clickhouse = clickhouse_connect.get_client(host=config.db_host, database=config.db_database, port=8123,
+                                           username=config.db_user, password=config.db_password,
+                                           settings={'async_insert': '1', 'wait_for_async_insert': '0'})
+
+
+def get_last_id_from_clickhouse(chat_id):
+    result = clickhouse.query(f"""
+        SELECT max(event_id) AS last_id
+        FROM admin_actions
+        WHERE chat_id = %(chat_id)s
+    """, parameters={"chat_id": chat_id}).result_rows
+    return result[0][0] if result and result[0][0] is not None else 0
+
+
+async def fetch_channel_actions(client, chat_id):
+    last_id = get_last_id_from_clickhouse(chat_id)
+    channel = await client.get_entity(chat_id)
+    max_id = last_id
+    new_last_id = last_id
+    all_data = []
+
+    while True:
+        events = await client(GetAdminLogRequest(
+            channel=channel,
+            q='',
+            min_id=max_id + 1,
+            max_id=0,
+            limit=100
+        ))
+
+        if not events.events:
+            break
+
+        for entry in events.events:
+            eid = entry.id
+            user_id = getattr(entry.user_id, 'user_id', entry.user_id)
+            action_type = type(entry.action).__name__
+            message = json.dumps(entry.action.to_dict(), default=str)
+
+            all_data.append([
+                eid,
+                chat_id,
+                action_type,
+                user_id or 0,
+                entry.date,
+                message
+            ])
+
+            if eid > new_last_id:
+                new_last_id = eid
+
+        if len(events.events) < 100:
+            break
+        else:
+            max_id = max(e.id for e in events.events)
+
+    if all_data:
+        clickhouse.insert('admin_actions', all_data, [
+            'event_id',
+            'chat_id',
+            'action_type',
+            'user_id',
+            'date',
+            'message'
+        ])
+        logging.info(f"[{datetime.utcnow()}] [{chat_id}] Inserted {len(all_data)} entries. Last ID: {new_last_id}")
+    else:
+        logging.info(f"[{datetime.utcnow()}] [{chat_id}] No new entries.")
--- src/main.py
+++ src/main.py
@@ -1,38 +1,51 @@
 import asyncio
 import logging
+import os
 import threading
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from flask import Flask, jsonify
-from telethon import events
 
-from config import config
-from notify_admins import notify_admins
-from scrapper import save_outgoing, save_incoming, save_deleted
 from TelegramUtils import create_telegram_client
+from admin_logs import fetch_channel_actions
+from config import config
 
 app = Flask(__name__)
+
+
 @app.route("/health")
 def health():
     resp = jsonify(health="healthy")
     resp.status_code = 200
     return resp
 
+
 def run_flask():
     app.run(host='0.0.0.0', port=80)
+
 
 async def run_telegram_clients():
     client = create_telegram_client('session/alex', config.telephone)
 
-    # Добавляем обработчики событий для клиента 1
-    client.add_event_handler(save_outgoing, events.NewMessage(outgoing=True))
-    client.add_event_handler(save_deleted, events.MessageDeleted())
-    client.add_event_handler(save_incoming, events.NewMessage(incoming=True))
-    client.add_event_handler(notify_admins, events.NewMessage(outgoing=True, pattern='!n', forwards=False))
+    # client.add_event_handler(save_outgoing, events.NewMessage(outgoing=True))
+    # client.add_event_handler(save_deleted, events.MessageDeleted())
+    # client.add_event_handler(save_incoming, events.NewMessage(incoming=True))
+    # client.add_event_handler(notify_admins, events.NewMessage(outgoing=True, pattern='!n', forwards=False))
+
+    scheduler = AsyncIOScheduler()
+
+    chat_ids_str = os.getenv("TELEGRAM_CHAT_IDS", "")
+    chat_ids = [int(cid.strip()) for cid in chat_ids_str.split(",") if cid.strip().isdigit()]
+    for chat_id in chat_ids:
+        scheduler.add_job(fetch_channel_actions, 'interval', minutes=1, args=[client, chat_id])
+
+    scheduler.start()
 
     # Запускаем клиентов
     await client.start()
 
     await client.run_until_disconnected()
+
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
