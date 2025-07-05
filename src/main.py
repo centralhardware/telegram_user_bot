b20b41af9eaf890aafbc21595f7c501ba435e3ac
alex <alex@centralhardware.me>
2025-07-05 19:44:04 +0800

    increase batch size

--- src/main.py
+++ src/main.py
@@ -35,18 +35,36 @@ def run_flask():
 
 async def run_telegram_clients():
     main_client = TelegramClient("session/alex", config.api_id, config.api_hash)
+    second_client = second_client = TelegramClient("session/alex2", config.api_id, config.api_hash,)
 
+    # Handlers for the primary client
+    main_client.add_event_handler(save_outgoing, events.NewMessage(outgoing=True))
+    main_client.add_event_handler(save_deleted, events.MessageDeleted())
+    main_client.add_event_handler(save_incoming, events.NewMessage(incoming=True))
+    second_client.add_event_handler(save_incoming, events.NewMessage(incoming=True))
+    
+    main_client.add_event_handler(handle_catbot_trigger, events.NewMessage())
 
+    scheduler = AsyncIOScheduler()
+
+    chat_ids = [int(cid.strip()) for cid in config.chat_ids if cid.strip().isdigit()]
+    for chat_id in chat_ids:
+        scheduler.add_job(
+            fetch_channel_actions, "interval", minutes=1, args=[main_client, chat_id]
+        )
+    scheduler.add_job(fetch_user_sessions, "interval", minutes=1, args=[main_client])
+    scheduler.add_job(flush_incoming_batch, "interval", seconds=10)
+    scheduler.start()
+
+    await main_client.start(phone=config.telephone)
+    await second_client.start(phone=config.telephone_second)
+    await asyncio.gather(
+        main_client.run_until_disconnected(),
+        second_client.run_until_disconnected(),
+    )
 
 
 def main():
-    second_client = TelegramClient(
-        "session/alex2",
-        config.api_id,
-        config.api_hash,
-    )
-    second_client.start()
-
     logging.basicConfig(level=logging.INFO, format="%(message)s")
     logging.getLogger("apscheduler").setLevel(logging.WARNING)
     logging.info("start application")
