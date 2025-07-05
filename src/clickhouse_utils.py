e195b6bc335efee600469b0dce0dd29420bacb49
alex <alex@centralhardware.me>
2025-07-05 16:27:54 +0800

    Revert "increase async_insert_flush_interval_milliseconds"
    This reverts commit d5bb383bc0cdf4d783c071177d63349ac67867df.

--- src/clickhouse_utils.py
+++ src/clickhouse_utils.py
@@ -14,6 +14,6 @@ def get_clickhouse_client():
             port=8123,
             username=config.db_user,
             password=config.db_password,
-            settings={"async_insert": "1", "wait_for_async_insert": "0", "async_insert_flush_interval_milliseconds": "10000"},
+            settings={"async_insert": "1", "wait_for_async_insert": "0"},
         )
     return _clickhouse_client
