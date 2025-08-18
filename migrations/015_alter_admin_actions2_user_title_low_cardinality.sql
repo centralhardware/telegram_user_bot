3c33edf13bf301677218981c412af560d1ba43f5
alex <alex@centralhardware.me>
2025-08-18 14:02:13 +0800

    migrate user_title/usernames to low cardinality for admin_actions2

---
+++ migrations/015_alter_admin_actions2_user_title_low_cardinality.sql
@@ -0,0 +1,2 @@
+ALTER TABLE telegram_user_bot.admin_actions2
+MODIFY COLUMN user_title LowCardinality(String);
---
+++ migrations/016_alter_admin_actions2_usernames_low_cardinality.sql
@@ -0,0 +1,2 @@
+ALTER TABLE telegram_user_bot.admin_actions2
+MODIFY COLUMN usernames Array(LowCardinality(String));
